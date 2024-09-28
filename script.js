234fd73b280fc93558d8ab3b589e10465cbc8da8
Maksim Sevostyanov <maksim.sevostyanov@simbirsoft.com>
2024-09-29 00:20:38 +0400

    added calculate

--- script.js
+++ script.js
@@ -1,1 +1,81 @@
-console.log('1')
+const ref = {
+  output: document.querySelector('.output'),
+  clear: document.querySelector('.clear'),
+  numbers: document.querySelector('.numbers'),
+  calculations: document.querySelector('.calculations'),
+}
+
+let value = ''
+let lastCalculation = ''
+
+const splitBySymbol = (str, symbol) => {
+  const result = str.split(symbol)
+
+  return result.map((element) => Number(element))
+}
+
+const handleClear = () => {
+  value = ''
+  ref.output.innerHTML = value
+}
+
+const handleNumbers = (e) => {
+  const currentSymbol = e.target.dataset.symbol
+  if (!currentSymbol) {
+    return
+  }
+
+  value += currentSymbol
+  ref.output.innerHTML = value
+}
+
+const handleCalculations = (e) => {
+  const currentSymbol = e.target.dataset.symbol
+  if (!currentSymbol) {
+    return
+  }
+
+  if (currentSymbol === '=') {
+    const result = splitBySymbol(value, lastCalculation)
+
+    switch (lastCalculation) {
+      case '÷':
+        value = result.reduce((acc, element, index) => {
+          if (index === 0) {
+            return (acc = element)
+          }
+
+          return (acc = acc / element)
+        }, 0)
+        break
+
+      case 'x': {
+        value = result.reduce((acc, element, index) => {
+          if (index === 0) {
+            return (acc = element)
+          }
+
+          return (acc = acc * element)
+        }, 0)
+        break
+      }
+
+      case '-':
+        value = result.reduce((acc, element, index) => (acc -= element), 0)
+        break
+
+      case '+':
+        value = result.reduce((acc, element) => (acc += element), 0)
+        break
+    }
+  } else {
+    value += currentSymbol
+  }
+
+  lastCalculation = currentSymbol
+  ref.output.innerHTML = value
+}
+
+ref.clear.addEventListener('click', handleClear)
+ref.numbers.addEventListener('click', handleNumbers)
+ref.calculations.addEventListener('click', handleCalculations)
